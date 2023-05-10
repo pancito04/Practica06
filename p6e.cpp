c00c924bbdaa373f7aafd11095dc9097f2e3ed08
Tony Jimenez <84590027+gertony@users.noreply.github.com>
2023-05-10 00:57:56 -0500

    Update p6e.cpp

--- p6e.cpp
+++ p6e.cpp
@@ -1,12 +1,34 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
-class Alumno{
-        string nombre;
-        int edad;
-        double promedio;
+
+class Alumno {
+public:
+    string nombre;
+    int edad;
+    double promedio;
+
+    Alumno(string nombre, int edad, double promedio) {
+        this->nombre = nombre;
+        this->edad = edad;
+        this->promedio = promedio;
+    }
+
+    friend ostream& operator<<(ostream& os, const Alumno& alumno);
+    friend bool operator<(const Alumno& alumno1, const Alumno& alumno2);
 };
+
+ostream& operator<<(ostream& os, const Alumno& alumno) {
+    os << "[" << alumno.nombre << ", " << alumno.edad << ", " << alumno.promedio << "]" << endl;
+    return os;
+}
+
+bool operator<(const Alumno& alumno1, const Alumno& alumno2) {
+    return alumno1.promedio < alumno2.promedio;
+}
+
 class Grupo{
         vector<Alumno>alumnos;
         int cantidad;
