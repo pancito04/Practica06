c6ae0261a81dad467d17c8ffd7f6363082443f01
erivero96 <108624162+erivero96@users.noreply.github.com>
2023-05-10 18:05:22 -0500

    Update exp3p6.cpp

--- exp3p6.cpp
+++ exp3p6.cpp
@@ -1,66 +1,117 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 using namespace std;
 
-class Banco {
-private:
-    static double tasaDeInteres;    //Creacion de atributo estatico
-
+class Alumno {
 public:
-    //Creacion de métodos estaticos 
-    static void setTasaDeInteres(double nuevaTasa) {
-        tasaDeInteres = nuevaTasa;
+    string nombre;
+    int edad;
+    double promedio;
+
+    Alumno(string nombre, int edad, double promedio) {
+        this->nombre = nombre;
+        this->edad = edad;
+        this->promedio = promedio;
     }
 
-    static double getTasaDeInteres() {
-        return tasaDeInteres;
-    }
+    friend ostream& operator<<(ostream& os, const Alumno& alumno);
+    friend bool operator<(const Alumno& alumno1, const Alumno& alumno2);
 };
 
-double Banco::tasaDeInteres = 0;
+ostream& operator<<(ostream& os, const Alumno& alumno) {
+    os << "[" << alumno.nombre << ", " << alumno.edad << ", " << alumno.promedio << "]" << endl;
+    return os;
+}
 
-class CuentaBancaria {
-private:
-    double saldo;
+bool operator<(const Alumno& alumno1, const Alumno& alumno2) {
+    return alumno1.promedio < alumno2.promedio;
+}
 
+class Grupo {
 public:
-    CuentaBancaria(double saldoInicial = 0) : saldo(saldoInicial) {}
-
-    void depositar(double cantidad) {
-        saldo += cantidad;
+    static int size1;
+    vector<Alumno> alumnos;
+    int cantidad;
+    
+    Grupo() {
+        cantidad = 0;
     }
 
-    void retirar(double cantidad) {
-        if (saldo >= cantidad) {
-            saldo -= cantidad;
-        } else {
-            cout << "Saldo insuficiente" << endl;
+
+    void agregarAlumno(string nombre, int edad, double promedio) {
+        if (size1 > cantidad){
+            Alumno nuevoAlumno(nombre, edad, promedio);
+            alumnos.push_back(nuevoAlumno);
+            cantidad++;
+        }
+
+        else{
+            cout << "Se ha intentado crear mas de "<<size1<<" objetos." <<endl;
+        }
+
+    }
+
+    void mostrarAlumnos() {
+        for (int i = 0; i < cantidad; i++) {
+            cout <<"Nommbre: "<< alumnos[i].nombre << " Edad: " << alumnos[i].edad << " Nota: " << alumnos[i].promedio << endl;
         }
     }
 
-    double calcularInteresGanado() const {
-        return saldo * Banco::getTasaDeInteres();
+    void ordenarPorPromedio() {
+        for (int i = 0; i < cantidad; i++) {
+            for (int j = i + 1; j < cantidad; j++) {
+                if (alumnos[j].promedio < alumnos[i].promedio) {
+                    swap(alumnos[i], alumnos[j]);
+                }
+            }
+        }
     }
 
-    double obtenerSaldo() const {
-        return saldo;
+    double promedioPromedios() {
+        double sumaPromedios = 0.0;
+        for (int i = 0; i < cantidad; i++) {
+            sumaPromedios += alumnos[i].promedio;
+        }
+        return sumaPromedios / cantidad;
+    }
+
+    Alumno mejorPromedio() {
+        Alumno mejor = alumnos[0];
+        for (int i = 1; i < cantidad; i++) {
+            if (alumnos[i].promedio > mejor.promedio) {
+                mejor = alumnos[i];
+            }
+        }
+        return mejor;
     }
 };
 
+int Grupo::size1 =5;
+
 int main() {
-    Banco::setTasaDeInteres(0.05);  // Tasa de interes del 5%
+    Grupo grupo;
+    grupo.agregarAlumno("Juan", 18, 17);
+    grupo.agregarAlumno("Guchi", 19, 11);
+    grupo.agregarAlumno("Luis", 20, 14);
+    grupo.agregarAlumno("Kikin", 18, 16);
+    grupo.agregarAlumno("Chorosor",19,19);
+    grupo.agregarAlumno("Juli0", 19, 19);
 
-    // Cracion de la cuenta bancaria y realizar operaciones
-    CuentaBancaria cuenta(1000);
-    cuenta.depositar(500);
-    cuenta.retirar(200);
+    cout << "Alumnos: " << endl<<"====================================="<< endl;
+    grupo.mostrarAlumnos();
+    cout <<"====================================="<< endl;
+    grupo.ordenarPorPromedio();
+    cout << "Alumnos ordenados por promedio: " << endl;
+    for (int i = 0; i < grupo.cantidad; i++) {
+        cout << grupo.alumnos[i];
+    }
+    cout <<"====================================="<< endl;
+    cout << "Promedio de todos los promedios: "<< grupo.promedioPromedios() << endl;
 
-    // Calculo del interes ganado por la cuenta
-    double interesGanado = cuenta.calcularInteresGanado();
-
-    cout << "Saldo actual: S/" << cuenta.obtenerSaldo() << endl;
-    cout << "Interés ganado: S/" << interesGanado << endl;
+    Alumno mejorPromedio = grupo.mejorPromedio();
+    cout << "Mejor promedio: "<< endl <<"Nombre: " << mejorPromedio.nombre << " Edad: " << mejorPromedio.edad << " Nota: " << mejorPromedio.promedio<<endl;
 
     return 0;
 }
-
